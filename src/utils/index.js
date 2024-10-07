f3b333650a13f87c0e25a28a0b778eba012523ce
kikiAgustin <kikiagustin62@gmail.com>
2024-10-07 20:57:30 +0700

    export notes implemented

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,19 +1,21 @@
-const mapDBToModel = ({ 
+const mapDBToModel = ({
+  id,
+  title,
+  body,
+  tags,
+  created_at,
+  updated_at,
+  username,
+}) => {
+  return {
     id,
     title,
     body,
     tags,
-    created_at,
-    updated_at
-  }) => {
-    return {
-      id,
-      title,
-      body,
-      tags,
-      createdAt: created_at,
-      updatedAt: updated_at,
-    };
-  }
+    createdAt: created_at,
+    updatedAt: updated_at,
+    username,
+  };
+};
 
-  module.exports = { mapDBToModel };
+module.exports = { mapDBToModel };
